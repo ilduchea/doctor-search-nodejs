9ed4951b8f8b12ea5f8d7fca85b263de8b80de60
Ilduchea <ilduchea@gmail.com>
2017-05-26 12:08:11 -0700

    add click function to listed doctors.

--- js/doctor.js
+++ js/doctor.js
@@ -6,24 +6,24 @@ function Doctor() {
 
 Doctor.prototype.getDoctors = function(medicalIssue, displayResults) {
   // api call
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
- .then(function(result) {
-    var doctors = [];
-    result.data.forEach(function(doctor) {
-      doctors.push(doctor.profile);
-    });
-    displayResults(doctors);
-  })
- .fail(function(error){
-    console.log("fail");
-  });
+ //  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+ // .then(function(result) {
+ //    var doctors = [];
+ //    result.data.forEach(function(doctor) {
+ //      doctors.push(doctor.profile);
+ //    });
+ //    displayResults(doctors);
+ //  })
+ // .fail(function(error){
+ //    console.log("fail");
+ //  });
 
-  //using example data
-  // var doctors = [];
-  // example.data.forEach(function(doctor) {
-  //   doctors.push(doctor.profile);
-  // });
-  // displayResults(doctors);
+  // using example data
+  var doctors = [];
+  example.data.forEach(function(doctor) {
+    doctors.push(doctor.profile);
+  });
+  displayResults(doctors);
 };
 
 exports.doctorModule = Doctor;
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,9 +1,30 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
-function displayResults(array) {
-  array.forEach(function(doctor) {
-    $('.results').append(`<p>${doctor.first_name}</p>`);
+function displayName(doctors) {
+  var i = 1;
+
+  doctors.forEach(function(doctor) {
+    $('.results').append(`<div class="doc${i}"></div>`);
+    $('div').last().append(`<h4 class="doc${i}">${doctor.first_name} ${doctor.last_name} ${doctor.title}</h4>`);
+    $(`h4.doc${i}`).click(function() {
+      $(`h4.doc${i} + div`).toggle();
+    });
+    i++;
   });
+}
+
+function displayDetails(doctors) {
+  var i = 1;
+
+  doctors.forEach(function(doctor) {
+    $(`div.doc${i}`).append(
+      ``
+    );
+  })
+}
+
+function displayResults(doctors) {
+  displayName(doctors);
 }
 
 $(function(){
